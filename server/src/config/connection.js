3a0b22b03f4ed668d09d94aa3534075eb11f4a9f
abalara4 <abalara4@gmail.com>
2024-12-04 21:45:29 -0500

    troubleshoot

--- server/src/config/connection.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-mongoose_1.default.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-exports.default = mongoose_1.default.connection;
--- server/src/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = express_1.default.Router();
-var user_routes_js_1 = require("./user-routes.js");
-router.use('/users', user_routes_js_1.default);
-exports.default = router;
